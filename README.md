a899b2a557bbc9d27a31724d2160ab9ea22da276
Knownymous <ankur.kothiwal99@gmail.com>
2019-04-05 09:15:33 +0530

    Adds initial code

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Pong written in C using SDL2
 
 1. To build use `make`
 2. To clean the object files use `make clean`
+# Multiplayer_Pong
--- src/input.c
+++ src/input.c
@@ -30,6 +30,39 @@ void doKeyUp(SDL_KeyboardEvent *event)
     }
 }
 
+//
+void doKeyDown2(SDL_KeyboardEvent *event)
+{
+    if (event->repeat == 0)
+    {
+        if (event->keysym.scancode == SDL_SCANCODE_W)
+        {
+            app.up = 1;
+        }
+        if (event->keysym.scancode == SDL_SCANCODE_S)
+        {
+            app.down = 1;
+        }
+    }
+}
+
+void doKeyUp2(SDL_KeyboardEvent *event)
+{
+    if (event->repeat == 0)
+    {
+        if (event->keysym.scancode == SDL_SCANCODE_W)
+        {
+            app.up = 0;
+        }
+        if (event->keysym.scancode == SDL_SCANCODE_S)
+        {
+            app.down = 0;
+        }
+    }
+}
+
+
+//
 void doInput(void)
 {
     SDL_Event event;
@@ -38,19 +71,19 @@ void doInput(void)
     {
         switch (event.type)
         {
-
         case SDL_QUIT:
             exit(0);
             break;
 
         case SDL_KEYDOWN:
             doKeyDown(&event.key);
+            doKeyDown2(&event.key);
             break;
 
         case SDL_KEYUP:
             doKeyUp(&event.key);
+            doKeyUp2(&event.key);
             break;
-
         default:
             break;
         }
