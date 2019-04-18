275821414d0e2d71b55007ca477f7b3b000ebc49
Divyansh Kamboj <kambojdivyansh2000@gmail.com>
2019-04-18 19:46:11 +0530

    adds a scoring system

--- src/main.c
+++ src/main.c
@@ -17,6 +17,7 @@ int main(int argc, char *argv[]) {
   player1.r = 255;
   player1.g = 0;
   player1.b = 0;
+  player1.Score = 0;
 
   player2.rect.x = 950;                       // x coordinate of rectangle
   player2.rect.y = (SCREEN_HEIGHT - 100) / 2; // y coordinate of rectange
@@ -26,6 +27,7 @@ int main(int argc, char *argv[]) {
   player2.r = 0;
   player2.g = 0;
   player2.b = 255;
+  player2.Score = 0;
 
   int start_ball = (rand() % (SCREEN_HEIGHT - 0 + 1));
   ball.rect.x = SCREEN_WIDTH / 2;
@@ -36,7 +38,7 @@ int main(int argc, char *argv[]) {
   ball.b = 0;
   ball.g = 255;
 
-  ball.x_vel = 8;
+  ball.x_vel = -8;
   ball.y_vel = 8;
 
   initSDL();       // Intialise
@@ -46,30 +48,32 @@ int main(int argc, char *argv[]) {
   drawEntity(player1);
   drawEntity(player2);
   presentScene();
-
-  SDL_Delay(3000);
+  SDL_Delay(2400);
+  
+  int balls = 3;
 
   while (1) {
     prepareScene();
     doInput();
+
     // controls for player 1
     // up
     if (app.w && (player1.rect.y - 10) >= 0) {
-      player1.rect.y -= 10;
+      player1.rect.y -= 15;
     }
     // down
     if (app.s && (player1.rect.y + player1.rect.h + 30) <= SCREEN_HEIGHT) {
-      player1.rect.y += 10;
+      player1.rect.y += 15;
     }
 
     // controls for player 2
     // up
     if (app.up && (player2.rect.y - 10) >= 0) {
-      player2.rect.y -= 10;
+      player2.rect.y -= 25;
     }
     // down
     if (app.down && (player2.rect.y + player2.rect.h + 30) <= SCREEN_HEIGHT) {
-      player2.rect.y += 10;
+      player2.rect.y += 25;
     }
 
     ball.rect.x += ball.x_vel;
@@ -89,6 +93,11 @@ int main(int argc, char *argv[]) {
         (ball.rect.y + ball.rect.w >= player1.rect.y &&
          ball.rect.y <= player1.rect.y + player1.rect.h)) {
       ball.x_vel = -ball.x_vel;
+      if (ball.x_vel < 0){
+        ball.x_vel -= 5;
+      } else{
+        ball.x_vel += 5;
+      }
     }
 
     // checks if the ball hit's player 2's paddle
@@ -96,18 +105,39 @@ int main(int argc, char *argv[]) {
         (ball.rect.y + ball.rect.w >= player2.rect.y &&
          ball.rect.y <= player2.rect.y + player2.rect.h)) {
       ball.x_vel = -ball.x_vel;
+        if (ball.x_vel < 0){
+          ball.x_vel -= 5;
+        } else{
+        ball.x_vel += 5;
+        }    
     }
 
     // if the ball touches the vertical walls the game ends
-    if (ball.rect.x + ball.rect.w >= SCREEN_WIDTH || ball.rect.x <= 0) {
-      break;
+    if (ball.rect.x + ball.rect.w >= SCREEN_WIDTH ) {
+      player1.Score ++;
+      balls--;
+      int start_ball = (rand() % (SCREEN_HEIGHT - 0 + 1));
+      ball.rect.x = SCREEN_WIDTH / 2;
+      ball.rect.y = start_ball;
+      SDL_Delay(1000);
+      ball.x_vel = 5;
+    } else if(ball.rect.x <= 0){
+      player2.Score ++;
+      balls--;
+      int start_ball = (rand() % (SCREEN_HEIGHT - 0 + 1));
+      ball.rect.x = SCREEN_WIDTH / 2;
+      ball.rect.y = start_ball;
+      SDL_Delay(1000);
+      ball.x_vel = 5;
     }
 
+    if (balls <= 0) break;
     drawBall(ball);
     drawEntity(player1);
     drawEntity(player2);
     presentScene();
     SDL_Delay(16);
   }
+  printf("%d, %d",player1.Score,player2.Score);
   return 0;
 }
