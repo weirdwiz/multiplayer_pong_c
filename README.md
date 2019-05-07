d572ac34ee25691958dbfa677187adc7a1b58ebd
Knownymous <ankur.kothiwal99@gmail.com>
2019-05-07 09:53:02 +0530

    Adds final Changes

--- README.md
+++ README.md
@@ -2,13 +2,7 @@
 
 This project aims to build a local network multiplayer pong game in C using SDL2
 
-<<<<<<< HEAD
 ## Building instructions
 
 1. `make` builds the game and gives out the binary
 2. `make clean` cleans out all the object files, etc.
-=======
-1. To build use `make`
-2. To clean the object files use `make clean`
-# Multiplayer_Pong
->>>>>>> a899b2a... Adds initial code
--- src/input.c
+++ src/input.c
@@ -34,63 +34,25 @@ void doKeyUp(SDL_KeyboardEvent *event) {
   }
 }
 
-//
-void doKeyDown2(SDL_KeyboardEvent *event)
-{
-    if (event->repeat == 0)
-    {
-        if (event->keysym.scancode == SDL_SCANCODE_W)
-        {
-            app.up = 1;
-        }
-        if (event->keysym.scancode == SDL_SCANCODE_S)
-        {
-            app.down = 1;
-        }
-    }
-}
+void doInput(void) {
+  SDL_Event event;
 
-void doKeyUp2(SDL_KeyboardEvent *event)
-{
-    if (event->repeat == 0)
-    {
-        if (event->keysym.scancode == SDL_SCANCODE_W)
-        {
-            app.up = 0;
-        }
-        if (event->keysym.scancode == SDL_SCANCODE_S)
-        {
-            app.down = 0;
-        }
-    }
-}
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_QUIT:
+      exit(0);
+      break;
 
+    case SDL_KEYDOWN:
+      doKeyDown(&event.key);
+      break;
 
-//
-void doInput(void)
-{
-    SDL_Event event;
+    case SDL_KEYUP:
+      doKeyUp(&event.key);
+      break;
 
-    while (SDL_PollEvent(&event))
-    {
-        switch (event.type)
-        {
-        case SDL_QUIT:
-            exit(0);
-            break;
-
-        case SDL_KEYDOWN:
-            doKeyDown(&event.key);
-            doKeyDown2(&event.key);
-            break;
-
-        case SDL_KEYUP:
-            doKeyUp(&event.key);
-            doKeyUp2(&event.key);
-            break;
-        default:
-            break;
-        }
+    default:
+      break;
     }
   }
 }
